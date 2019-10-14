3d5e9c6fe6d6cbc6332109f4df604e73994a23a6
Karin Norrlander <karin@barprego.se>
2019-10-14 21:53:36 +0200

    Utvecklar metod animalNeedsToEat

--- Klasser.cs
+++ Klasser.cs
@@ -11,19 +11,29 @@ namespace humans_and_animals
         public string namn;
         public int hungrig = 0;
 
+        public int foodleft = 0;
+
+        //Metod för att skriva ut djuren som finns
         public virtual void printAnimal()
         {
             Console.WriteLine();
         }
-
-        public void raiseHungerLevel() 
+        //Metod räknar upp hungernivån på djuret
+        public void raiseHungerLevel()
         {
-            hungrig ++;
+            hungrig++;
         }
+
+        //Metod för varje djur som:
+        //Ökar hungernivån
+        //Kollar om djuret behöver äta eller inte
+        //Konsumerar mat och återställer hungernivån
+
         public virtual void animalNeedsToEat()
         {
             Console.WriteLine();
         }
+
     }
 
     //subklass för elefant 
@@ -39,19 +49,29 @@ namespace humans_and_animals
         {
             if (hungrig == 10)
             {
-                Console.WriteLine("Elefanten " + namn + " är hungrig och behöver äta");
-                Console.WriteLine("Tack! Nu äter elefanten " + namn + " och är snart mätt igen");
-                hungrig = 0;
+                if (Program.nrOfVeggiesLeft >= 1)
+                {
+                    Console.WriteLine("Elefanten " + namn + " är hungrig och behöver äta");
+                    Console.WriteLine("Tack! Nu äter elefanten " + namn + " och är snart mätt igen");
+                    hungrig = 0;
+
+                    Program.nrOfVeggiesLeft--;
+                    Console.WriteLine("Det finns nu " + Program.nrOfVeggiesLeft + " blad kvar.");
+                }
+                else
+                {
+                    Console.WriteLine("Nu är bladen slut. Elefanten " + namn + " är hungrig men kan inte få nån mat!");
+                }
+            }
+            else 
+            {
+                Console.WriteLine (sort + " " + namn + " behöver inte äta idag.");
             }
         }
     }
 
     class Giraff : Djur
     {
-        //Du behöver skriva en metod för varje djur som gör följande:
-        //* Ökar hungernivån
-        //* Kollar om djuret behöver äta eller inte
-        //* Konsumerar mat och återställer hungernivån
         protected string mat = "blad";
         public string Mat { get { return mat; } }
         public override void printAnimal()
@@ -62,9 +82,23 @@ namespace humans_and_animals
         {
             if (hungrig == 7)
             {
-                Console.WriteLine("Giraffen " + namn + " är hungrig och behöver äta");
-                Console.WriteLine("Tack! Nu äter giraffen " + namn + " och är snart mätt igen");
-                hungrig = 0;
+                if (Program.nrOfVeggiesLeft >= 1)
+                {
+                    Console.WriteLine("Giraffen " + namn + " är hungrig och behöver äta");
+                    Console.WriteLine("Tack! Nu äter giraffen " + namn + " och är snart mätt igen");
+                    hungrig = 0;
+
+                    Program.nrOfVeggiesLeft--;
+                    Console.WriteLine("Det finns nu " + Program.nrOfVeggiesLeft + " blad kvar.");
+                }
+                else
+                {
+                    Console.WriteLine("Nu är bladen slut. Giraffen" + namn + " är hungrig men kan inte få mat. Gör ett inköp!");
+                }
+            }
+            else 
+            {
+                Console.WriteLine (sort + " " + namn + " behöver inte äta idag.");
             }
         }
     }
@@ -72,13 +106,31 @@ namespace humans_and_animals
     {
         protected string mat = "kött";
         public string Mat { get { return mat; } }
+        public override void printAnimal()
+        {
+            Console.WriteLine(sort + " | " + namn + " | " + mat);
+        }
         public override void animalNeedsToEat()
         {
             if (hungrig == 13)
             {
-                Console.WriteLine("Sälen " + namn + " är hungrig och behöver äta");
-                Console.WriteLine("Tack! Nu äter sälen " + namn + " och är snart mätt igen");
-                hungrig = 0;
+                if (Program.nrOfMeatLeft >= 1)
+                {
+                    Console.WriteLine("Sälen " + namn + " är hungrig och behöver äta");
+                    Console.WriteLine("Tack! Nu äter sälen " + namn + " och är snart mätt igen");
+                    hungrig = 0;
+
+                    Program.nrOfMeatLeft--;
+                    Console.WriteLine("Det finns nu " + Program.nrOfMeatLeft + " köttbitar kvar.");
+                }
+                else
+                {
+                    Console.WriteLine("Nu är köttet slut! Sälen " + namn + " är hungrig. Det måste köpas in ny mat!");
+                }
+            }
+            else 
+            {
+                Console.WriteLine (sort + " " + namn + " behöver inte äta idag.");
             }
         }
     }
@@ -94,9 +146,22 @@ namespace humans_and_animals
         {
             if (hungrig == 3)
             {
-                Console.WriteLine("Björnen " + namn + " är hungrig och behöver äta");
-                Console.WriteLine("Tack! Nu äter björnen " + namn + " och är snart mätt igen");
-                hungrig = 0;
+                if (Program.nrOfMeatLeft >= 1)
+                {
+                    Console.WriteLine("Björnen " + namn + " är hungrig och behöver äta");
+                    Console.WriteLine("Tack! Nu äter björnen " + namn + " och är snart mätt igen");
+                    hungrig = 0;
+                    Program.nrOfMeatLeft--;
+                    Console.WriteLine("Det finns nu " + Program.nrOfMeatLeft + " köttbitar kvar.");
+                }
+                else
+                {
+                    Console.WriteLine("Det finns inget kött kvar. Björnen " + namn + " är hungrig. Det måste köpas in nytt");
+                }
+            }
+            else 
+            {
+                Console.WriteLine (sort + " " + namn + " behöver inte äta idag.");
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -6,21 +6,23 @@ namespace humans_and_animals
 
     class Program
     {
+        public static int nrOfMeatLeft = 10;
+        public static int nrOfVeggiesLeft = 10;
         static void Main(string[] args)
         {
             List<Djur> anmials = new List<Djur>();
 
-            Elefant minElefant = new Elefant();
-            minElefant.sort = "Elefant";
-            minElefant.namn = "Karin";
-            minElefant.hungrig = 0;
-            anmials.Add(minElefant);
-
             Seal minSeal = new Seal();
             minSeal.sort = "Säl";
-            minSeal.namn = "Penny";
+            minSeal.namn = "Buster";
             minSeal.hungrig = 0;
             anmials.Add(minSeal);
+
+            Elefant minElefant = new Elefant();
+            minElefant.sort = "Elefant";
+            minElefant.namn = "Hanna";
+            minElefant.hungrig = 0;
+            anmials.Add(minElefant);
 
             Bear minBear = new Bear();
             minBear.sort = "Björn";
@@ -28,28 +30,20 @@ namespace humans_and_animals
             minBear.hungrig = 0;
             anmials.Add(minBear);
 
-            Elefant majsElefant = new Elefant();
-            majsElefant.sort = "Blå Elefant";
-            majsElefant.namn = "Bumbo";
-            majsElefant.hungrig = 0;
-            anmials.Add(majsElefant);
+            Giraff minGiraff = new Giraff();
+            minGiraff.sort = "Giraff";
+            minGiraff.namn = "Pricken";
+            minGiraff.hungrig = 0;
+            anmials.Add(minGiraff);
 
-            Bear minAndraBear = new Bear();
-            minAndraBear.namn = "Bertil";
-            minAndraBear.sort = "Björn";
-            minAndraBear.hungrig = 0;
-            anmials.Add(minAndraBear);
-
-            Elefant tredjeElefanten = new Elefant();
-            tredjeElefanten.namn = "Pluto";
-            tredjeElefanten.sort = "Elefant";
-            tredjeElefanten.hungrig = 0;
-            anmials.Add(tredjeElefanten);
-            tredjeElefanten.animalNeedsToEat();
+            Seal secondSeal = new Seal();
+            secondSeal.sort = "Säl";
+            secondSeal.namn = "Doris";
+            secondSeal.hungrig = 0;
+            anmials.Add(secondSeal);
 
             // Day loop
-            int nrOfMeatLeft = 50;
-            int nrOfVeggiesLeft = 50;
+            
             int day = 0;
             bool thereIsFoodLeft = true;
 
@@ -59,33 +53,34 @@ namespace humans_and_animals
                 {
                     djur.printAnimal();
                 }
+            
+            Console.ReadKey();
 
             while (thereIsFoodLeft)
             {
                 day++;
                 Console.WriteLine("Det är dag " + day + ":");
                 Console.WriteLine("-------");
+                
 
                 foreach (Djur djur in anmials)
                 {
                     djur.raiseHungerLevel();
                    
                     djur.animalNeedsToEat();
+
+                    if (nrOfMeatLeft ==0 && nrOfVeggiesLeft ==0)
+                    {
+                        Console.WriteLine("Maten är slut! Ni skulle väl köpa in mat!? Vad har ni egentligen gjort?!");
+                        thereIsFoodLeft = false;
+                    }
+
                 }
-                
 
                 // LÄGG TILL DIN NYA KOD HÄR
 
                 // Wait for the next day
                 Console.ReadKey();
-
-                //Kollar om maten har tagit slut:
-                // if (nrOfMeatLeft = 0)
-                // {
-                //     Console.WriteLine("Det är dag " + day + "och maten har nu tagit slut. SLUT");
-                //     thereIsFoodLeft = false;
-                //     Console.WriteLine("Programmet kommer nu att avsltas.");
-                // }
             }
         }
     }
